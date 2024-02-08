3d8c9e813854ba11c2e6e495af5eea1baa387f77
Your Name <kaischenkel@gmail.com>
2024-02-07 17:18:34 -0800

    adding Finishing Touches

--- Algorithm 4/Mergesort_and_Quicksort/src/Main.java
+++ Algorithm 4/Mergesort_and_Quicksort/src/Main.java
@@ -15,8 +15,9 @@ public class Main {
         Scanner terminalScanner = new Scanner(System.in);
         Mergesort_KS sortFunction = new Mergesort_KS();
         Quicksort_KS quickSortFunction = new Quicksort_KS();
+        boolean programmeIsRunning = true;
         try {
-            File inputFile = new File("numbers-4.txt");
+            File inputFile = new File("src/numbers-4.txt");
             Scanner fileScanner = new Scanner(inputFile);
             
             int spaceForArray = 0;
@@ -42,38 +43,76 @@ public class Main {
             }
             fileScanner.close();
             System.out.printf("Unsorted array: %s%n%n", Arrays.toString(numberFromFile));
-            
-            quickSortFunction.quickSort(numberFromFile, 0, numberFromFile.length - 1);
-            System.out.printf("Sorted array using Quick sort: %s%n%n", Arrays.toString(numberFromFile));
-            int value1 = indexReturn(numberFromFile, 90262);
-            int value2 = indexReturn(numberFromFile, 11559);
-            //Check if the first asked position is in the array  
-            if(value1 == -1){
-                System.out.println("The value 90262 was not found in the array");
-            } else {
-                System.out.println("The value 90262 was found at index " + value1);
+
+           //This is the interface for the user to interact with the program
+            while (programmeIsRunning) {
+                Scanner inputScanner = new Scanner(System.in);
+                printMenu();
+                String inputFromTerminal = terminalScanner.nextLine();
+               
+                switch (inputFromTerminal.charAt(0)) {
+                    case 'p':
+                        System.out.printf("Here is the array Printed: %s%n%n", Arrays.toString(numberFromFile));
+                        break;
+                        
+                    case 's':
+                        System.out.println("[]=============================================[]");
+                        System.out.println("[] Sorting the array using Quick sort...       []");
+                        quickSortFunction.quickSort(numberFromFile, 0, numberFromFile.length - 1);
+                        System.out.println("[] The array has been sorted using Quick sort! []");
+                        System.out.println("[]=============================================[]");
+                        break;
+
+                    case 'm':
+                        System.out.println("[]=============================================[]");
+                        System.out.println("Sorting the array using Merge sort...          []");
+                        sortFunction.mergeSort(numberFromFile);
+                        System.out.println("The array has been sorted using Merge sort!    []");
+                        System.out.println("[]=============================================[]");
+                        break;
+
+                    case 'f':
+                        System.out.println("[]==============================================================[]");
+                        System.out.println("[] Enter a value to find in the array and will return the index []");
+                        System.out.printf("[] Enter your choice -> ");
+                        int valueToFind = inputScanner.nextInt(); 
+                        int valueAtIndex = indexReturn(numberFromFile, valueToFind);
+                        if(valueAtIndex == -1){
+                            System.out.printf("[] The value %d was not found in the array                 []\n", valueToFind);
+                            System.out.println("[]==============================================================[]");
+                        } else {
+                            System.out.printf("[] The value %d was found at index %d                      []\n", valueToFind, valueAtIndex);
+                            System.out.println("[]==============================================================[]");
+                        }
+                        break;
+                    case 'q':
+                        System.out.println("Shutting down... Goodbye world");
+                        programmeIsRunning = false;
+                        inputScanner.close();
+                        break;
+                
+                    default:
+                        break;
+                }
+                
             }
-            //Check if the second asked position is in the array
-            if(value2 == -1){
-                System.out.println("The value 11559 was not found in the array");
-            } else {
-                System.out.println("The value 11559 was found at index " + value2);
-            }
+            terminalScanner.close();
         } catch (Exception e) {
             System.out.println("An error occured: " + e);
+            programmeIsRunning = false;
         }
     }
 
-    private static int indexReturn(int[] numList, int targetIndex) {
+    private static int indexReturn(int[] numList, int targetValue) {
         //Just a simple binary search algorithm beacuse we know that the array will be sorted in ascending order
         int leftIndex = 0;
         int rightIndex = numList.length - 1;
 
         while (leftIndex <= rightIndex) {
             int middleIndex = (leftIndex + rightIndex) / 2;
-            if (numList[middleIndex] == targetIndex) {
+            if (numList[middleIndex] == targetValue) {
                 return middleIndex;
-            } else if (numList[middleIndex] < targetIndex) {
+            } else if (numList[middleIndex] < targetValue) {
                 leftIndex = middleIndex + 1;
             } else {
                 rightIndex = middleIndex - 1;
@@ -82,4 +121,17 @@ public class Main {
         //If the value is not found in the array we return -1
         return -1;
     }
+    private static void printMenu() {
+        //Per print statement only have two choices in each statement 
+        System.out.printf("[]==========================================================[]\n");
+        System.out.printf("[] Enter 'p' to print the array                             []\n");
+        System.out.printf("[] Enter 's' to sort the array using Quick sort             []\n");
+        System.out.printf("[] Enter 'm' to sort the array using Merge sort             []\n");
+        System.out.printf("[] Enter 'f' to find a index of a Value                     []\n");   
+        System.out.printf("[] Enter 'q' to quit                                        []\n");
+        System.out.printf("[]==========================================================[]\n");
+        System.out.printf("[] Enter your choice -> ");  
+    }
+
+    
 }
--- Algorithm 4/Mergesort_and_Quicksort/src/Mergesort_KS.java
+++ Algorithm 4/Mergesort_and_Quicksort/src/Mergesort_KS.java
@@ -9,82 +9,51 @@ Sources: Deitel, Deitel Book, small bit of chatGPT for error handleing and Geeks
 
 public class Mergesort_KS {
     public void mergeSort(int[] arrayData){
+        if (arrayData.length == 0) {
+            System.out.println("Array is empty");
+            return;
+        }
         sortArray(arrayData, 0, arrayData.length - 1);
     }
 
     private void sortArray(int[] data, int lowIndex, int highIndex) {
-        if ((highIndex - lowIndex) >= 1) {
+        if (lowIndex < highIndex){
             int middleValue1 = (lowIndex + highIndex) / 2;
             int middleValue2 = middleValue1 + 1;
-
-            //output that we are splitting the array up
-            System.out.printf("Array split: %s%n", subArrayString(data, lowIndex, highIndex).toString());
-            System.out.printf("   %s%n", subArrayString(data, lowIndex, middleValue1));
-            System.out.printf("   %s%n%n", subArrayString(data, middleValue2, highIndex));
 
             sortArray(data, lowIndex, middleValue1);
             sortArray(data, middleValue2, highIndex);
 
             mergeArrays(data, lowIndex, middleValue1, middleValue2, highIndex);
-        }
-        
-    }
-
-    private String subArrayString(int[] arrayData, int lowValue, int highIndex) {
-        StringBuilder temporatyString = new StringBuilder();
-
-        //Output spaces for alignment
-        for (int i = 0; i < lowValue; i++) {
-            temporatyString.append("   ");
-        }
-        //Output elements left in array
-        for (int i = lowValue; i <= highIndex; i++) {
-            temporatyString.append(" " + arrayData[i]);
-        }
-       return temporatyString.toString();
+        }   
     }
 
     private void mergeArrays(int[] arrayData, int lowIndex, int middleValue1, int middleValue2, int highIndex) {
         int leftIndex = lowIndex;
         int rightIndex = middleValue2;
-        int combinedIndex = lowIndex;
-        int [] combinedArrays = new int[arrayData.length];
-
-        //Print the arrays before the mergeing process
-        System.out.printf("merge: %s%n", subArrayString(arrayData, lowIndex, middleValue1));
-
-        System.out.printf("      %s%n", subArrayString(arrayData, middleValue2, highIndex));
+        int temporaryIndex = 0;
+        int [] combinedArrays = new int[highIndex - lowIndex + 1];
 
         //Merge the arrays until reaching the end of either array
         while (leftIndex <= middleValue1 && rightIndex <= highIndex) {
             /*We ask the computer to place the smaller of the two current elements,
              into the result and move to the next space in the array*/
             if (arrayData[leftIndex] <= arrayData[rightIndex]) {
-                combinedArrays[combinedIndex++] = arrayData[leftIndex++];
+                combinedArrays[temporaryIndex++] = arrayData[leftIndex++];
             } else {
-                combinedArrays[combinedIndex++] = arrayData[rightIndex++];
+                combinedArrays[temporaryIndex++] = arrayData[rightIndex++]; 
             }
         }
 
-        //If the left array is empty
-        if (leftIndex == middleValue2) {
-            while (rightIndex <= highIndex) {
-                combinedArrays[combinedIndex++] = arrayData[rightIndex++];
-            }
-        } else {
-            while (leftIndex <= middleValue1) {
-                combinedArrays[combinedIndex++] = arrayData[leftIndex++];
-            }
+        while (leftIndex <= middleValue1) {
+            combinedArrays[temporaryIndex++] = arrayData[leftIndex++];
         }
 
-        //Copy values back into the original array that was passed in
-        for (int i =  lowIndex; i <= highIndex; i++) {
-            arrayData[i] = combinedArrays[i];
+        while (rightIndex <= highIndex) {
+            combinedArrays[temporaryIndex++] = arrayData[rightIndex++];  
         }
 
-        //Output the merged array
-        System.out.printf("   %s%n%n", subArrayString(arrayData, lowIndex, highIndex));
-
+        System.arraycopy(combinedArrays, 0, arrayData, lowIndex, combinedArrays.length);
     }
     
 }
