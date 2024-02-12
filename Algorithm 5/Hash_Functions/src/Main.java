aa918699d3778b68bce8fcb2e6f154c82071d9d6
Your Name <kaischenkel@gmail.com>
2024-02-12 12:45:22 -0800

    Update GitHub

--- Algorithm 5/Hash_Functions/src/Main.java
+++ Algorithm 5/Hash_Functions/src/Main.java
@@ -6,8 +6,29 @@ Algorithms HW 5 : Hash functions
 File Name: Main.java
 Sources: Deitel, Deitel Book, small bit of chatGPT for error handleing
 */
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) throws Exception {
-        System.out.println("");
+        boolean programStillRunning = true; // Main loop control
+        Scanner tremminalScanner = new Scanner(System.in);
+        while(programStillRunning){ // Menu While loop
+            String userInput = tremminalScanner.nextLine();
+            switch (userInput.charAt(0)) {
+                case 'h':
+                    System.out.println("Hello, World!");
+                    break;
+                
+                case 'q':
+                    programStillRunning = false;
+                    break;
+
+                default:
+                    break;
+            }
+        }
+        tremminalScanner.close();
     }
+    
 }
