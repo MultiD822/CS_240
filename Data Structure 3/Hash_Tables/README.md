8ed9581c4626e86716d60a86a7b22da4fbb9375e
Your Name <kaischenkel@gmail.com>
2024-02-12 11:12:27 -0800

    Just set up files

---
+++ Data Structure 3/Hash_Tables/README.md
@@ -0,0 +1,8 @@
+# Data Structures 3: Hash tables
+-You will build the following data structure in the programming language of your choice. 
+
+## Hash Table 
+Implement a hash table  with hash function of your choice.  You should turn in two types of hash tables, with some way to handle collisions.  
+
+Have fun and good luck!
+
--- Data Structure 3/Hash_Tables/src/Main.java
+++ Data Structure 3/Hash_Tables/src/Main.java
@@ -2,12 +2,12 @@
 Programmer: Kai Schenkel
 Class: CS 240
 Data: 2/12/2024
-Algorithms HW 5 : Hash functions
+Data Strutures 3: Hash functions
 File Name: Main.java
 Sources: Deitel, Deitel Book, small bit of chatGPT for error handleing
 */
 public class Main {
     public static void main(String[] args) throws Exception {
-        System.out.println("");
+        System.out.println("Hello, World!");
     }
 }
