961d5834f870834533016dff9c6b2d578c84257f
Your Name <kaischenkel@gmail.com>
2024-02-14 00:27:27 -0800

    Just adding things

--- Algorithm 6/BinaryTrees_Search&Sort/src/App.java
+++ Algorithm 6/BinaryTrees_Search&Sort/src/App.java
@@ -1,5 +1,71 @@
+/*
+Programmer: Kai Schenkel
+Class: CS 240
+Data: 2/13/2024
+Algorithms 6: Binary Trees - Search & Sort
+File Name: Main.java
+Sources: Deitel, Deitel Book, small bit of chatGPT for error handleing, plus the file from Algorithms 5 Hash functions
+    the class file is called KL_
+*/
+
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        boolean programStillRunning = true; // Main loop control
+        Binary_Tree Binary_Tree = new Binary_Tree(null);
+        Scanner tremminalScanner = new Scanner(System.in);
+        System.out.println("Welcome to the Binary Tree Program!");
+        while (programStillRunning) {
+            printOptions();
+            String userInput = tremminalScanner.nextLine();
+            switch (userInput.charAt(0)) {
+                
+                case 'a':
+                    System.out.println("Please enter the value of the node you would like to add to the tree");
+                    int value = tremminalScanner.nextInt();
+                    Node newNode = new Node(value);
+                    Binary_Tree.add(newNode);
+                    break;
+                
+                case 'r':
+                    System.out.println("Please enter the value of the root node");
+                    int rootValue = tremminalScanner.nextInt();
+                    Node newRoot = new Node(rootValue);
+                    Binary_Tree.rootNode = newRoot;
+                    break;
+
+                case 'd':
+                    System.out.println("Please enter the value of the node you would like to delete from the tree");
+                    int deleteValue = tremminalScanner.nextInt();
+                    Binary_Tree.deleteNode(newRoot, deleteValue);
+                    break;
+                
+                case 'q':
+                    programStillRunning = false;
+                    break;
+                
+                default:
+                    System.out.printf("[]======================================[]\n");
+                    System.out.printf("[]That Seems to do Nothing... Weird     []\n");
+                    System.out.printf("[]======================================[]\n");
+                    break;
+            }
+        }
+        tremminalScanner.close();
+    }
+
+    public static void printOptions() {
+        System.out.println("[]================================================[]");
+        System.out.println("[] 'a' Add element to the tree                    []");
+        System.out.println("[] 'r' Set the root of the Binary Tree            []");
+        System.out.println("[] 'd' Delete a element from the Binary Tree      []");
+        System.out.println("[] 's' Search for a element in the Binary Tree    []");
+        System.out.println("[] 'p' Pre-order traversal                         []");
+        System.out.println("[] 'i' in-order traversal                         []");
+        System.out.println("[] 'o' Post-order traversal                       []");
+        System.out.println("[] 'l' Print the Binary Tree                      []");
+        System.out.println("[] 'q' Exit the program                           []");
+        System.out.println("[]================================================[]");
     }
 }
--- Algorithm 6/BinaryTrees_Search&Sort/src/Binary_Tree.java
+++ Algorithm 6/BinaryTrees_Search&Sort/src/Binary_Tree.java
@@ -1,3 +1,84 @@
+/*
+Programmer: Kai Schenkel
+Class: CS 240
+Data: 2/13/2024
+Algorithms 6: Binary Trees - Search & Sort
+File Name: Binary_Tree.java
+Sources: Deitel, Deitel Book, small bit of chatGPT for error handleing, plus the file from Algorithms 5 Hash functions
+    the class file is called KL_
+*/
+
+
+//This is the Binary Tree class
 public class Binary_Tree {
-    
+     Node rootNode;
+
+    public Binary_Tree(Node root) {
+        this.rootNode = root;
+    }
+
+    public void add(Node node) {
+        if (this.rootNode == null) {
+            this.rootNode = node;
+        } else {
+            this.addExisting(node, this.rootNode);
+        }
+    }
+
+    private void addExisting(Node node, Node current) {
+        if (node.value > current.value) {
+            if (current.right == null) {
+                current.right = node;
+            } else {
+                addExisting(node, current.right);
+            }
+        } else if (node.value < current.value) {
+            if (current.left == null) {
+                current.left = node;
+            } else {
+                addExisting(node, current.left);
+            }
+        }
+    }
+    //Delete a node
+    public void delete(int value) {
+        this.rootNode = deleteNode(this.rootNode, value);
+    }
+    //In-order traversal
+    public void inOrder(Node node) {
+        if (node != null) {
+            inOrder(node.left);
+            System.out.print(node.value + " ");
+            inOrder(node.right);
+        }
+    }
+    //Post-order traversal
+    public void postOrder(Node node) {
+        if (node != null) {
+            postOrder(node.left);
+            postOrder(node.right);
+            System.out.print(node.value + " ");
+        }
+    }
+    //Pre-order traversal
+    public void preOrder(Node node) {
+        if (node != null) {
+            System.out.print(node.value + " ");
+            preOrder(node.left);
+            preOrder(node.right);
+        }
+    }
+}    
+//My own Node class
+class Node {
+    int value;
+    Node left;
+    Node right;
+
+    public Node(int value) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
 }
+
