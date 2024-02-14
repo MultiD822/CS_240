5c0f4247c4c56b2205061a44f7a1ce52ac73fd8e
Your Name <kaischenkel@gmail.com>
2024-02-13 18:26:43 -0800

    Done with Algorithms 5

--- Algorithm 5/Hash_Functions/src/KL_Hash_Functions.java
+++ Algorithm 5/Hash_Functions/src/KL_Hash_Functions.java
@@ -1,11 +1,40 @@
+/*
+Programmer: Kai Schenkel
+Class: CS 240
+Data: 2/13/2024
+Algorithms HW 5 : Hash functions
+File Name: KL_Hash_Functions.java
+Sources: Deitel, Deitel Book, small bit of chatGPT for error handleing, GeekforGeeks website
+*/
 public class KL_Hash_Functions {
-    
-    public static int hashFunction(String input, int base, int capacity){
+    //
+    public int hashFactory(String input, int base, int capacity){
+        if (input.isEmpty()){
+            return 0;
+        }
         int hash = 0;
         int length = input.length();
         for (int i = 0; i < length; i++) {
             hash = (base * hash + input.charAt(i)) % capacity;
         }
         return hash;
+    }
+
+    public boolean isHashPrime(int hashValue){
+        if (hashValue <= 1) return false;
+        if (hashValue <= 3) return true;
+        if (hashValue % 2 == 0 || hashValue % 3 == 0) return false;
+        for (int i = 5; i * i <= hashValue; i += 6){
+            if (hashValue % i == 0 || hashValue % (i + 2) == 0) return false;
+        }
+        return true;
+    }
+
+    public int nearestBigPrime (int hashValue) {
+        hashValue = hashValue % 2 == 0 ? hashValue + 1 : hashValue + 2; //Sets hash value to the next odd number
+        while (!isHashPrime(hashValue)) {
+            hashValue += 2; // Only checks odd numbers
+        }
+        return hashValue;
     }
 }
--- Algorithm 5/Hash_Functions/src/Main.java
+++ Algorithm 5/Hash_Functions/src/Main.java
@@ -1,42 +1,57 @@
 /*
 Programmer: Kai Schenkel
 Class: CS 240
-Data: 2/12/2024
+Data: 2/13/2024
 Algorithms HW 5 : Hash functions
 File Name: Main.java
-Sources: Deitel, Deitel Book, small bit of chatGPT for error handleing
+Sources: Deitel, Deitel Book, small bit of chatGPT for error handleing, GeekforGeeks website
 */
 
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
+        KL_Hash_Functions hashFunctions = new KL_Hash_Functions();
         boolean programStillRunning = true; // Main loop control
-        int base = 31;
-        int capacity = 100;
+        int base = 63;
+        int capacity = Integer.MAX_VALUE;
         Scanner tremminalScanner = new Scanner(System.in);
         while(programStillRunning){ // Menu While loop
             printMenu();
             String userInput = tremminalScanner.nextLine();
             switch (userInput.charAt(0)) {
-                case 'h':
-                    System.out.println("Hello, World!");
+                case 'n':
+                    System.out.printf("Please enter a the hash value: ");
+                    int findPrime = tremminalScanner.nextInt();
+                    int primeFound = hashFunctions.nearestBigPrime(findPrime);
+                    tremminalScanner.nextLine();
+                    System.out.printf("The next biggest Prime is %d\n", primeFound);
                     break;
                 
-                case 't':
+                case 'h':
                     System.out.println("Please enter a string to hash: ");
                     String input = tremminalScanner.nextLine();
-                    int hash = hashFunction(input, base, capacity);
+                    int hash = hashFunctions.hashFactory(input, base, capacity);
                     System.out.println("The hash of " + input + " is " + hash);
                     break; 
                 
                 case 'p':
-                
+                System.out.printf("Please enter a the hash value: ");
+                int primeInput = tremminalScanner.nextInt();
+                boolean testedPrime = hashFunctions.isHashPrime(primeInput);
+                tremminalScanner.nextLine();
+                    if (testedPrime == true){
+                        System.out.printf("The hash value is a Prime number\n");
+                    } else {
+                        System.out.printf("The hash value is not a Prime number\n");
+                    }
+                    break;
                 case 'q':
                     programStillRunning = false;
                     break;
 
                 default:
+                    System.out.printf("That Seems to do Nothing... Weird\n");
                     break;
             }
         }
@@ -45,18 +60,9 @@ public class Main {
     
     public static void printMenu(){
         System.out.println("Please enter a command: ");
-        System.out.println("h: Hello World");
-        System.out.println("t: Test Hash Function");
-        System.out.println("p: Print Hash Table");
+        System.out.println("n: Nerst Big Prime");
+        System.out.println("h: Test Hash Function");
+        System.out.println("p: Check if Prime");
         System.out.println("q: Quit");
-    }
-
-    public static int hashFunction(String input, int base, int capacity){
-        int hash = 0;
-        int length = input.length();
-        for (int i = 0; i < length; i++) {
-            hash = (base * hash + input.charAt(i)) % capacity;
-        }
-        return hash;
     }
 }
